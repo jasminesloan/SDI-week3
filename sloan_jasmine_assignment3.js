985845bba9d97a469836f78dba20192163844fe7
Jasmine Jamieson <Jasmine@unknown28cfdaee4738.gateway.2wire.net>
2012-04-11 23:36:58 -0400

    Added example codes

--- sloan_jasmine_assignment3.js
+++ sloan_jasmine_assignment3.js
@@ -1,1 +1,18 @@
-console.log("Added line of code in my branch")
+var pirate = {
+    realName:    "Jean Lafitte",
+    rank:        "Captain",
+    privateer:   true,
+    ships:       [ "La Diligent", "Pride" ],
+    sailTo:      function (destination) {},
+    getLocation: function () {}
+};
+pirate.sailTo("Tortuga");
+var pirate = { /* stuff here */ },
+    ship   = { /* more stuff here */ },
+    target = "Tortuga";
+pirate.announce("Our target today is " + target + ".");
+var myName = pirate.getRealName();    // accessor method
+pirate.announce("We'll find plunder, or my name isn't " + myName + "!");
+pirate.sailTo(target);                // mutator + procedure method
+var damage = pirate.attack(target);   // function method
+pirate.repairDamage(damage, ship);    // object argument
